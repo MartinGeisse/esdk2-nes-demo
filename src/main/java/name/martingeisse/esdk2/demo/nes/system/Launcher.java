558b7147f28e6d3fd9753a42747b6d31e82bf5c8
Martin Geisse <martingeisse@googlemail.com>
2018-07-12 22:59:52 +0200

    cleanup

--- src/main/java/name/martingeisse/esdk2/demo/nes/system/Launcher.java
+++ src/main/java/name/martingeisse/esdk2/demo/nes/system/Launcher.java
@@ -1,7 +1,7 @@
-/**
- * Copyright (c) 2013 Shopgate GmbH
+/*
+ * Copyright (c) 2018 Martin Geisse
+ * This file is distributed under the terms of the MIT license.
  */
-
 package name.martingeisse.esdk2.demo.nes.system;
 
 import org.lwjgl.input.Keyboard;
