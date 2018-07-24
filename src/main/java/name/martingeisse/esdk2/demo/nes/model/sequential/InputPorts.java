7f2510c05f18784cb34b78f4be4af2e4de6173e1
Martin Geisse <martingeisse@googlemail.com>
2018-07-24 22:51:42 +0200

    implemented controller 1 input -- seems to work

--- src/main/java/name/martingeisse/esdk2/demo/nes/model/sequential/InputPorts.java
+++ src/main/java/name/martingeisse/esdk2/demo/nes/model/sequential/InputPorts.java
@@ -4,21 +4,49 @@
  */
 package name.martingeisse.esdk2.demo.nes.model.sequential;
 
+import org.lwjgl.input.Keyboard;
+
 /**
  *
  */
 public final class InputPorts {
 
+	private boolean strobe;
+	private int latchedController1Data;
+
 	public void write16(byte data) {
-		// TODO
+		if (strobe) {
+			latchController1Data();
+		}
+		strobe = (data & 1) != 0;
 	}
 
 	public byte read16() {
-		// TODO
+		if (strobe) {
+			latchController1Data();
+			return (byte) (latchedController1Data & 1);
+		} else {
+			byte result = (byte) (latchedController1Data & 1);
+			latchedController1Data >>>= 1;
+			return result;
+		}
+	}
+
+	private void latchController1Data() {
+		latchedController1Data = 0;
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_UP) ? 16 : 0);
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_DOWN) ? 32 : 0);
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_LEFT) ? 64 : 0);
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_RIGHT) ? 128 : 0);
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_X) ? 1 : 0); // A
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_C) ? 2 : 0); // B
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_RETURN) ? 8 : 0); // start
+		latchedController1Data |= (Keyboard.isKeyDown(Keyboard.KEY_SPACE) ? 4 : 0); // select
 	}
 
 	public byte read17() {
-		// TODO
+		// no controller 2 attached
+		return 0;
 	}
 
 }
